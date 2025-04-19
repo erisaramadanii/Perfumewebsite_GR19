2c6f10edfb9ea2cb5440759fc52401594e15ceae
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-04-19 03:10:39 +0200

    Update offers.php

--- offers.php
+++ offers.php
@@ -19,4 +19,21 @@ $parfumet = [
 new Parfum(emri: "So Scandal", foto: "F1_So_Scandal.jpg", cmimi: 78, gjinia: "female"),
   new Parfum(emri: "La Male", foto: "M5_La_Male.jpg", cmimi: 60, gjinia: "male"),
   new Parfum(emri: "La Male Le Parfum", foto: "M2_La_Male_Le_Parfum.jpg", cmimi: 75, gjinia: "male"),
-  
+   new Parfum("La Beau", "M3_La_Beau.webp", 65, "male"),
+    new Parfum("Ultra Male", "M4_Ultra_Male.jpg", 80, "male"),
+];
+
+$gjinia = isset($_GET['gjinia']) ? $_GET['gjinia'] : null;
+$sort = $_GET['sort'] ?? null;
+$gjiniaArray = is_array($gjinia) ? $gjinia : explode(',', $gjinia);
+
+if ($gjinia) {
+    $parfumet = array_filter($parfumet, fn($p) => in_array($p->gjinia, $gjiniaArray));
+}
+
+if ($sort === "asc") {
+    usort($parfumet, fn($a, $b) => $a->cmimi <=> $b->cmimi);
+} elseif ($sort === "desc") {
+    usort($parfumet, fn($a, $b) => $b->cmimi <=> $a->cmimi);
+}
+?>
