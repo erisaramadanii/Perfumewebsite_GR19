bc4143f1fbcc35ffd60d1b791b3fa515e25cc9de
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-04-19 02:13:32 +0200

    Update offers.php

--- offers.php
+++ offers.php
@@ -13,4 +13,10 @@ class Parfum{
 }
 }
 $parfumet = [
-  new Parfum(emri: "Classique", foto: 
+  new Parfum(emri: "Classique", foto: "F1_Classique.jpg", cmimi: 55, gjinia: "female"),
+  new Parfum(emri: "Scandal", foto: "F1_Scandal.jpg", cmimi: 70, gjinia: "female"),
+  new Parfum(emri: "La Belle", foto: "F1_La_Belle.webp.jpg", cmimi: 68, gjinia: "female"),
+new Parfum(emri: "So Scandal", foto: "F1_So_Scandal.jpg", cmimi: 78, gjinia: "female"),
+  new Parfum(emri: "La Male", foto: "M5_La_Male.jpg", cmimi: 60, gjinia: "male"),
+  new Parfum(emri: "La Male Le Parfum", foto: "M2_La_Male_Le_Parfum.jpg", cmimi: 75, gjinia: "male"),
+  
