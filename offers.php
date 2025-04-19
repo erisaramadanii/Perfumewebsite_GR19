c424fb14c5e513a60b90ccafb87ada1534b36011
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-04-19 02:04:26 +0200

    Update offers.php

--- offers.php
+++ offers.php
@@ -1,1 +1,14 @@
+<?php 
+class Parfum{
+  public $emri;
+  public $foto;
+  public $cmimi;
+  public $gjinia;
 
+  public function__construct($emri, $cmimi, $gjinia) {
+  $this->emri = $emri;
+  $this->foto = $foto;
+  $this->cmimi = $cmimi;
+  $this->gjinia = $gjinia;
+}
+}
