9b99ef2590a9137cdafb76523a3d63b6d9de3df0
Erisa Ramadani <erisa.ramadani@student.uni-pr.edu>
2025-05-15 15:41:18 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,7 @@
+<form method="POST" action="includes/send_email.php">
+    Email: <input type="email" name="to" required><br>
+    Subjekti: <input type="text" name="subject" required><br>
+    Mesazhi:<br>
+    <textarea name="message" required></textarea><br>
+    <input type="submit" value="Dërgo">
+</form>
