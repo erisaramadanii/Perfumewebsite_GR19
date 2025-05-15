e6a27f820a0647c433558068993aabf366428261
Erisa Ramadani <erisa.ramadani@student.uni-pr.edu>
2025-05-15 15:35:41 +0200

    Update error_handler.php

--- Includes/error_handler.php
+++ Includes/error_handler.php
@@ -1,1 +1,9 @@
+<?php
+function error_handler($errno, $errstr, $errfile, $errline) {
+    echo "<b>Gabim:</b> $errstr në $errfile në linjën $errline<br>";
+    error_log("Gabim [$errno]: $errstr në $errfile në linjën $errline\n", 3, __DIR__ . "/../logs/errors.log");
+    return true;
+}
+set_error_handler("error_handler");
+?>
 
