9fa4b88a1b1367cb95df743bc54ee1c5769df57a
delvinaelshani <delvina.elshani@student.uni-pr.edu>
2025-05-24 17:23:29 +0200

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="sq">
+<head>
+    <meta charset="UTF-8">
+    <title>Politika e Privatësisë</title>
+</head>
+<body>
+    <h2>Politika e Privatësisë</h2>
+    <p>Kjo faqe përshkruan mënyrën se si ne mbledhim, ruajmë dhe përdorim informacionin tuaj personal. Të dhënat nuk shpërndahen me palë të treta dhe përdoren vetëm për përmirësimin e përvojës në faqen tonë të parfumit <strong>Jean Paul Gaultier</strong>.</p>
+</body>
+</html>
