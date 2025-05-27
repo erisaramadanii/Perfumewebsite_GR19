f4896aba4651a78958776d4f25f7a1842b455c60
Erisa Ramadani <erisa.ramadani@student.uni-pr.edu>
2025-05-27 08:30:22 +0200

    Delete Includes directory

--- Includes/utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-function log_to_file($message) {
-    $logfile = __DIR__ . "/../logs/app.log";
-    $entry = date("Y-m-d H:i:s") . " - $message\n";
-    file_put_contents($logfile, $entry, FILE_APPEND);
-}
-?>
-
